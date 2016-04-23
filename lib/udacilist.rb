1d34e2356072cbb8d9c0ffea44ba66cb8f5e8abe
José Robles <joseroblesoneto@gmail.com>
2016-04-23 11:49:00 -0300

    chronic gem allows date input to be string relative to date, eg: in 2 days, error raised if one tries to add an item type not supported, colorize gem allows priority symbol to be red when high, yellow when medium and green when low

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -1,12 +1,22 @@
 class UdaciList
   attr_reader :title, :items
+  @@types = []
+  @@types.push('todo')
+  @@types.push('event')
+  @@types.push('link')
+
+  def self.types
+    @@types
+  end
 
   def initialize(options={})
-    @title = options[:title]
+    options[:title] ? @title  = options[:title] : @title = "Untitled List"
     @items = []
   end
   def add(type, description, options={})
     type = type.downcase
+    puts "InvalidItemType: type #{type} is not suported in UdaciList" if !@@types.include?(type)
+    #raise UdaciListErrors::InvalidItemType, "type #{type} is not suported in UdaciList" if !@@types.include?(type)
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
@@ -21,5 +31,11 @@ class UdaciList
     @items.each_with_index do |item, position|
       puts "#{position + 1}) #{item.details}"
     end
+  end
+
+  private
+
+  def add_type(type)
+    @@types.push(type) if !@@types.include?(type)
   end
 end
