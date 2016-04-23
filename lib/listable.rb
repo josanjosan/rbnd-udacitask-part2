63e57649410178054b8c659122cc5444a7d4aebd
José Robles <joseroblesoneto@gmail.com>
2016-04-23 10:04:37 -0300

    Refactor format_date and format_priority methods, now in Listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -4,4 +4,19 @@ module Listable
     "#{description}".ljust(25)
   end
 
+  def format_date(date_1, date_2 = nil)
+    date_1 ? dates = date_1.strftime("%D") : dates = "No due date"
+    dates << " -- " + date_2.strftime("%D") if date_2
+    dates = "N/A" if !dates
+    return dates
+  end
+
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
+
 end
